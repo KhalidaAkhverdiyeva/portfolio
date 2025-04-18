6d5c5cce812438401fc2e11efffed76bb4ef88d7
Your Name <you@example.com>
2025-04-18 20:57:50 +0400

    bug-fix: custom font bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,16 @@ export default function Home() {
       <div className="flex flex-1 gap-[20px] overflow-hidden mx-[20px] mb-[20px]">
         <div className="w-[70%] flex flex-col gap-[20px]">
           <div className="flex gap-[20px] flex-[2]">
-            <div className="bg-[#ece7e1] w-[70%] flex-[3] rounded-[20px]">
-              Welcome
+            <div className="font-['ClashDisplay'] font-[200] bg-[#ece7e1] text-[40px] flex items-end p-[20px] w-[70%] flex-[3] rounded-[20px] leading-tight">
+              <span className="text-[50px]">
+                {" "}
+                Hey, I’m Khalida! I’m a frontend developer
+              </span>
+              {/* <span className=" text-[40px] whitespace-nowrap">
+                who loves puzzles.
+              </span> */}
             </div>
+
             <div className=" w-[30%] flex-1 rounded-[20px] relative overflow-hidden">
               <Image
                 src="/image/bw.me.jpg"
@@ -23,15 +30,23 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="flex gap-[20px] flex-[1]">
-            <div className="bg-[#ece7e1] w-[50%] rounded-[20px]">Funnn</div>
-            <div className="bg-[#D1FD0A] w-[50%] rounded-[20px]">Contact</div>
+          <div className="flex gap-[20px] flex-[1] font-['Tanker-Regular']">
+            <div className="bg-[#ece7e1] font-['Tanker-Regular'] text-[40px] w-[50%] rounded-[20px] flex items-end p-[20px] ">
+              Funnn
+            </div>
+            <div className="bg-[#D1FD0A] font-['Tanker-Regular'] text-[60px] w-[50%] rounded-[20px] flex items-end px-[20px] ">
+              Contact me
+            </div>
           </div>
         </div>
 
         <div className="w-[30%] flex flex-col gap-[20px]">
-          <div className="bg-[#ece7e1] rounded-[20px] flex-[5]">Project</div>
-          <div className="bg-[#ece7e1] flex-[1] rounded-[20px]">Skills</div>
+          <div className="bg-[#ece7e1] rounded-[20px] flex-[5] flex items-end p-[20px] ">
+            Project
+          </div>
+          <div className="bg-[#ece7e1] flex-[1] rounded-[20px] flex items-end p-[20px] ">
+            Skills
+          </div>
         </div>
       </div>
     </div>
